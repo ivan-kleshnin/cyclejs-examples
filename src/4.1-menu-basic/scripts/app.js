d5ad06801aaea204a691a223c6ef32fb676e75a3
Ivan Kleshnin <ivan@paqmind.com>
2015-03-18 22:34:24 +0100

    Update Example #4.1

--- src/4.1-menu-basic/scripts/app.js
+++ src/4.1-menu-basic/scripts/app.js
@@ -1,49 +1,17 @@
 // IMPORTS =========================================================================================
 let Cycle = require("cyclejs");
 let {Rx, h} = Cycle;
-let makeClass = require("classnames");
+let Menu = require("./menu");
 
 // APP =============================================================================================
-Cycle.registerCustomElement("Menu", (DOM, Props) => {
-  let Model = Cycle.createModel(Intent => {
-    return {
-      active$: Intent.get("selectActive$").startWith(0),
-    };
-  });
+let data = [
+  "Home", "Services", "About", "Contact us"
+];
 
-  let View = Cycle.createView((Model, Props) => {
-    let active$ = Model.get("active$");
-    let items$ = Props.get("items$");
-    return {
-      vtree$: active$.combineLatest(items$, (active, items) => {
-        return (
-          <div>
-            <ul>
-              {items.map((item, i) =>
-                <li attributes={{"data-index": i}} key={i} class={makeClass({"menu-item": true, active: i == active})}>{item}</li>
-              )}
-            </ul>
-            <p>Selected: <b>{items[active]}</b></p>
-          </div>
-        );
-      }),
-      // TODO https://github.com/alexmingoia/jsx-transform/issues/15
-    };
-  });
+let active = "Services";
 
-  let Intent = Cycle.createIntent(DOM => {
-    return {
-      selectActive$: DOM.event$(".menu-item", "click").map(event => event.target.dataset.index),
-    };
-  });
-
-  DOM.inject(View).inject(Model, Props)[0].inject(Intent).inject(DOM);
-});
-
-let View = Cycle.createView(() => {
-  return {
-    vtree$: Rx.Observable.return(h("Menu", {items: ["Home", "Services", "About", "Contact us"]})),
-  };
-});
+let View = Cycle.createView(() => ({
+  vtree$: Rx.Observable.return(h("Menu", {items: data, active: active})),
+}));
 
 Cycle.createDOMUser("main").inject(View);
---
+++ src/4.1-menu-basic/scripts/menu.js
@@ -0,0 +1,51 @@
+// IMPORTS =========================================================================================
+let Cycle = require("cyclejs");
+let {Rx, h} = Cycle;
+let makeClass = require("classnames");
+
+// COMPONENTS ======================================================================================
+Cycle.registerCustomElement("Menu", (DOM, Props) => {
+  let Model = Cycle.createModel((Intent, Props) => {
+    let items$ = Props.get("items$");
+    let active$ = Props.get("active$");
+    let selectActive$ = Intent.get("selectActive$");
+    return {
+      items$: items$,
+      active$: active$.merge(selectActive$),
+    }
+  });
+
+  let View = Cycle.createView(Model => {
+    let items$ = Model.get("items$");
+    let active$ = Model.get("active$");
+    return {
+      vtree$: items$.combineLatest(active$, (items, active) => {
+        return (
+          <div>
+            <nav>
+              {items.map(item =>
+                <div attributes={{"data-name": item}} key={item}
+                  class={makeClass({"menu-item": true, active: item == active})}>
+                  {item}
+                </div>
+              )}
+            </nav>
+            <p>
+              Selected: <b>{active}</b>
+            </p>
+          </div>
+        );
+      }),
+      // TODO https://github.com/alexmingoia/jsx-transform/issues/15
+    };
+  });
+
+  let Intent = Cycle.createIntent(DOM => {
+    return {
+      selectActive$: DOM.event$(".menu-item", "click")
+        .map(event => event.currentTarget.dataset.name),
+    };
+  });
+
+  DOM.inject(View).inject(Model).inject(Intent, Props)[0].inject(DOM);
+});
