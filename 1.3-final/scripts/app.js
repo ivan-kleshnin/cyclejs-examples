a11885a0fed5508d80a2fc02dab44266d335a3aa
Ivan Kleshnin <ivan@paqmind.com>
2015-05-29 13:21:44 +0200

    Add example 1.3

---
+++ 1.3-final/scripts/app.js
@@ -0,0 +1,18 @@
+// IMPORTS =========================================================================================
+import Cycle from "cyclejs";
+let {Rx} = Cycle;
+let Observable = Rx.Observable;
+
+// APP =============================================================================================
+function Computer1() {
+  return Observable.just(<div>App #1 rendered!</div>);
+}
+
+function Computer2() {
+  return {
+    vtree$: Observable.just(<div>App #2 rendered!</div>),
+  };
+}
+
+Cycle.applyToDOM("#app-1", () => Computer1());
+Cycle.applyToDOM("#app-2", () => Computer2());
