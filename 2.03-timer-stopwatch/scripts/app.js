7ae27476d1dd1a1e15cbb372ffb2db9c7864e423
Ivan Kleshnin <ivan@paqmind.com>
2015-06-09 16:25:16 +0200

    Animate stopwatch

--- 2.03-timer-stopwatch/scripts/app.js
+++ 2.03-timer-stopwatch/scripts/app.js
@@ -6,7 +6,7 @@ let {Rx} = Cycle;
 let {Observable, Subject} = Rx;
 
 // CONSTANTS =======================================================================================
-const TICK_MS = 100;
+const TICK_MS = 500;
 const MINUTE_MS = 600 * TICK_MS;
 
 // APP =============================================================================================
@@ -22,12 +22,14 @@ function Model(intentions) {
     return {
       watch: 0,         // state: 0 = stopped, 1 = running, 2 = paused
       value: MINUTE_MS, // timer value in milliseconds
+      prevValue: MINUTE_MS,
     };
   }
 
   let trigger$ = intentions.trigger$.map(() => {
     return function (state) {
       if (state.watch == 2) {
+        state = assoc("prevValue", state.value, state);
         state = assoc("value", MINUTE_MS, state);
       }
       state = assoc("watch", (state.watch + 1) % 3, state);
@@ -56,7 +58,9 @@ function Model(intentions) {
     state$: transform$
       .scan(seedState(), (state, transform) => transform(state))
       .map(state => {
-        return assoc("value", MINUTE_MS - state.value, state);
+        state = assoc("prevValue", MINUTE_MS - state.prevValue, state);
+        state = assoc("value", MINUTE_MS - state.value, state);
+        return state;
       })
       .distinctUntilChanged()
       .shareReplay(1)
@@ -73,14 +77,19 @@ function View(state) {
   }
 
   return state.state$.map(state => {
+      let prevSecond = mSecToSec(state.prevValue);
       let second = mSecToSec(state.value);
       let angle = secondToAngle(second);
+      let stopped = state.watch == 0;
       return (
         <div>
           <div className="stopWatch frame">
-            <div className="stopWatch arrow" style={{
+            <div className={Class("stopWatch", "arrow")} style={{
               "transform": `rotate(${angle}deg)`,
               "transform-origin": "bottom",
+              "transition-property": "transform",
+              "transition-timing-function": stopped ? "ease-out" : "cubic-bezier(.4, 2, .55, .44)",
+              "transition-duration": stopped ? `${prevSecond / 30}s` : `${TICK_MS / 1000}s`,
             }}>
               {second}
             </div>
