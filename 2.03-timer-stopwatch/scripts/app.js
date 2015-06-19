257fbb0e7499dd6db5bdd43c78bd2b02162647a4
Ivan Kleshnin <ivan@paqmind.com>
2015-06-19 11:29:26 +0200

    Cleanup

--- 2.03-timer-stopwatch/scripts/app.js
+++ 2.03-timer-stopwatch/scripts/app.js
@@ -2,7 +2,7 @@ import {assoc} from "ramda";
 import Class from "classnames";
 import Cycle from "cyclejs";
 let {Rx} = Cycle;
-let {Observable, Subject} = Rx;
+let {Observable} = Rx;
 
 // CONSTANTS =======================================================================================
 const TICK_MS = 500;
@@ -74,30 +74,29 @@ function View(state) {
   }
 
   return state.state$.map(state => {
-      let secondBeforeReset = mSecToSec(state.valueBeforeReset);
-      let second = mSecToSec(state.value);
-      let angle = secondToAngle(second);
-      let stopped = state.watch == 0;
-      return (
-        <div>
-          <div className="stopWatch frame">
-            <div className={Class("stopWatch", "arrow")} style={{
-              "transform": `rotate(${angle}deg)`,
-              "transform-origin": "bottom",
-              "transition-property": "transform",
-              "transition-timing-function": stopped ? "ease-out" : "cubic-bezier(.4, 2, .55, .44)",
-              "transition-duration": stopped ? `${secondBeforeReset / 30}s` : `${TICK_MS / 1000}s`,
-            }}>
-              {second}
-            </div>
-          </div>
-          <div className="stopWatch trigger">
-            <button className="btn btn-default trigger">Trigger</button>
+    let secondBeforeReset = mSecToSec(state.valueBeforeReset);
+    let second = mSecToSec(state.value);
+    let angle = secondToAngle(second);
+    let stopped = state.watch == 0;
+    return (
+      <div>
+        <div className="stopWatch frame">
+          <div className={Class("stopWatch", "arrow")} style={{
+            "transform": `rotate(${angle}deg)`,
+            "transform-origin": "bottom",
+            "transition-property": "transform",
+            "transition-timing-function": stopped ? "ease-out" : "cubic-bezier(.4, 2, .55, .44)",
+            "transition-duration": stopped ? `${secondBeforeReset / 30}s` : `${TICK_MS / 1000}s`,
+          }}>
+            {second}
           </div>
         </div>
-      );
-    }
-  );
+        <div className="stopWatch trigger">
+          <button className="btn btn-default trigger">Trigger</button>
+        </div>
+      </div>
+    );
+  });
 }
 
 Cycle.applyToDOM("#main", interactions => View(Model(Intent(interactions))));
