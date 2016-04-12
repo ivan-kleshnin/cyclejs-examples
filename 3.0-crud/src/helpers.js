79a09929e7fec22d07bc571a9cb53247bf9696c8
Ivan Kleshnin <ivan@paqmind.com>
2016-04-12 14:52:38 +0300

    Update helpers

--- 3.0-crud/src/helpers.js
+++ 3.0-crud/src/helpers.js
@@ -1,5 +1,9 @@
 let R = require("ramda")
-let {curry, split} = require("ramda")
+let {curry, pipe, split, update} = require("ramda")
+
+let fst = (xs) => xs[0]
+
+let snd = (xs) => xs[1]
 
 // always :: a -> b -> a
 let always = curry((x, y) => x)
@@ -29,8 +33,45 @@ let adjustBy = curry((pred, adjustFn, array) => {
   }
 })
 
+let swap = curry((i1, i2, array) => {
+  let v1 = array[i1]
+  let v2 = array[i2]
+  return pipe(
+    update(i1, v2),
+    update(i2, v1)
+  )(array)
+})
+
+let randomInt = curry((min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min
+})
+
+let pickRandom = curry((array) => {
+  return array[randomInt(0, array.length)]
+})
+
+let shuffle = curry((array) => {
+  let counter = array.length
+  while (counter > 0) {
+    let index = randomInt(0, counter)
+    counter -= 1
+    array = swap(index, counter, array)
+  }
+  return array
+})
+
+exports.fst = fst
+exports.snd = snd
+
 exports.always = always
 exports.lens = lens
-exports.updateBy = updateBy
-exports.adjustBy = adjustBy
 
+exports.adjustBy = adjustBy
+exports.updateBy = updateBy
+
+exports.swap = swap
+
+exports.randomInt = randomInt
+exports.pickRandom = pickRandom
+
+exports.shuffle = shuffle
